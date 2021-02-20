87c372270552eea06479234e648f5dcfec0f03a9
alankstewart <alankstewart@gmail.com>
2021-02-20 17:07:40 +1100

    Add axios config js file

---
+++ frontend/src/http-common.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export const HTTP = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  headers: { 'Access-Control-Allow-Origin': 'http://localhost:8081' }
+})
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
+import { HTTP } from './http-common'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,9 +9,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
-
-Vue.prototype.$http = axios
-axios.defaults.baseURL = 'http://localhost:8080/api'
+Vue.prototype.$http = HTTP
 
 new Vue({
   router,
